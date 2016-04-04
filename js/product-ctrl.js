d606f3eb38983574d9fb3509f4975ef008d85e10
manasirao <mansirao@gmail.com>
2016-04-03 23:36:18 -0500

    WIP Product Filter

--- js/product-ctrl.js
+++ js/product-ctrl.js
@@ -2,17 +2,20 @@ var app = angular.module('gopikaApp');
 app.controller('productCtrl', function($scope, $http) {
 	console.log('Product Controller Initialized');
 	$scope.products = [];
+	$scope.box = false;
+    $scope.cone = false;
+    $scope.pouch = false;
     $http.defaults.cache = false;
 	
 	$scope.showInfo =function() {
 		console.log("On showInfo");
-	}
+	};
 	var getProductInfo = function() {
-			$http({method: 'GET', url: '/products.json'
+			$http({method: 'GET', url: '/products-new.json?nocache=' + (new Date()).getTime()
 			}).then(function successResp(response) {
 				console.log("Product Received: " + JSON.stringify(response));
 				$scope.products = response.data;
 			});
-	}
+	};
 	getProductInfo();
 });
---
+++ js/product-type-filter.js
@@ -0,0 +1,28 @@
+var app = angular.module('gopikaApp');
+
+app.filter('gpwProdType', function() {
+    console.log('Gopika Product Type Filter');
+
+    return function(input, isBox, isPouch, isCone) {
+        console.log('Filter: box:' + isBox + 'isPouch:' + isPouch + ' isCone:' + isCone);
+        var output = [];
+        if(isBox === false && isPouch === false && isCone === false ) {
+            output = input;
+            return output;
+        }
+        for (var i = 0; i < input.length; ++i) {
+            var product = input[i];
+
+            if (isBox && product.type === 'box') {
+                output.push(product);
+            }
+            if (isPouch && product.type === 'pouch') {
+                output.push(product);
+            }
+            if (isCone && product.type === 'cone') {
+                output.push(product);
+            }
+        }
+        return output;
+    }
+});
